f29f0124002f8c466631a744458b0108406e8779
honeyjim <honeyjimcui@gmail.com>
2023-01-11 13:13:00 +0800

    file permission to 777

--- src/CLogger/Handlers/RotationFileHandler.php
+++ src/CLogger/Handlers/RotationFileHandler.php
@@ -8,5 +8,8 @@ use Monolog\Handler\RotatingFileHandler as MonologRotationFileHandler;
 
 class RotationFileHandler extends MonologRotationFileHandler
 {
-
+    public function __construct(string $filename, int $maxFiles = 7, $level = Logger::DEBUG, bool $bubble = true, int $filePermission = 0777, bool $useLocking = false)
+    {
+        parent::__construct($filename, $maxFiles, $level, $bubble, $filePermission, $useLocking);
+    }
 }
