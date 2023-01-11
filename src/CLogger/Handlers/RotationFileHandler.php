eb10d63af62aef7ffaab4504ca416972e12707a3
honeyjim <honeyjimcui@gmail.com>
2023-01-11 13:34:58 +0800

    +use Monolog\Logger;

--- src/CLogger/Handlers/RotationFileHandler.php
+++ src/CLogger/Handlers/RotationFileHandler.php
@@ -4,6 +4,7 @@ declare (strict_types = 1);
 
 namespace Think\Clogger\Handlers;
 
+use Monolog\Logger;
 use Monolog\Handler\RotatingFileHandler as MonologRotationFileHandler;
 
 class RotationFileHandler extends MonologRotationFileHandler
